41cfd6e5612f6b4a2441e348ee44f858db001177
cneubauern <cneubauern@googlemail.com>
2016-03-11 00:17:46 +0100

    added collections, schemas, and functions

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -1,11 +1,9 @@
 Template.dashboard.onRendered(
-
-
-    function(){
+    function () {
 
         var username = Meteor.user().username;
 
-        if(typeof username === "undefined" ){
+        if (typeof username === "undefined") {
 
             console.log("undefined");
 
@@ -15,6 +13,30 @@ Template.dashboard.onRendered(
             console.log(username)
         }
 
-    });
+    }
+);
 
+Template.dashboard.helpers({
+
+    projectsCount: function () {
+
+        var count = UserProjects.find({userId: Meteor.userId()}).count();
+
+        var bool = Boolean;
+
+        if (count > 0){
+            bool = true;
+        }
+        else{
+            bool = false;
+        }
+
+        return bool;
+    },
+
+    projects: function () {
+        return UserProjects.find({userId: Meteor.userId()});
+    }
+
+})
 
